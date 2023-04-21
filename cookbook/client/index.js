0e3342c1e6a891d89fca4d961ff7ba4ca4f9245f
SyedAli1996 <104249620+SyedAli1996@users.noreply.github.com>
2023-04-21 18:32:19 -0400

    Update index.js

--- cookbook/client/index.js
+++ cookbook/client/index.js
@@ -36,4 +36,34 @@ function checkCredentials() {
   });
   
 
+const apiKey = 'YOUR_API_KEY_HERE';
+const prompt = document.getElementById('prompt').value;;
+
+const gpt3Promise = new Promise((resolve, reject) => {
+  fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      'prompt': prompt,
+      'max_tokens': 50,
+      'n': 1,
+      'stop': '\n'
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const result = data.choices[0].text.trim();
+    resolve(result);
+  })
+  .catch(error => reject(error));
+});
+
+gpt3Promise.then(result => {
+  console.log(result);
+}).catch(error => {
+  console.error(error);
+});
 
